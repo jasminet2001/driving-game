df80af366796216245ed57e52fe62a7d87297c47
Jasmine <jasminesr2001@gmail.com>
2023-12-16 21:55:10 +0330

    creating a car and fixing camera angle

---
+++ Driving-Game/Assets/Script/CarController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 5.0f;
+    private float forwardInput;
+    private float horizontalInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //void FixedUpdate()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        rb.AddRelativeForce(Vector3.forward);
+    //    }
+    //    if (Input.GetKey(KeyCode.S))
+    //    {
+    //        rb.AddRelativeForce(-Vector3.forward);
+    //    }
+    //}
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+
+}
